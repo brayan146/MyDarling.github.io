2152f9e30fd1fc0435782110fd09bf5bcd19e64f
brayan146 <brayan14717@gmail.com>
2024-08-05 23:01:31 -0400

    birthday web

---
+++ config.js
@@ -0,0 +1,49 @@
+
+var config = {
+    texts: [
+        "Cada momento contigo es un tesoro inigualable",      
+        "Contigo, cada día es una aventura maravillosa.",  
+        "Tu sonrisa ilumina mi vida.",
+        "Juntos, creamos recuerdos inolvidables.",
+        "Eres la pieza que altera mi vida.",
+        "Eres el caos adorable que amo en mi vida.",
+        "Hacerte enojar es mi deporte favorito.",
+        "Prometo seguir queriéndote, incluso cuando te pongas en modo 'drama queen'.",
+        "No hay nada más hermoso que verte feliz.",
+        "Te quiero a pesar de tus berrinches.",
+        "Eres una persona que se gano mi cariño.",
+        "Espero que te quedes a mi lado por mucho tiempo más. Porfi.",
+        "Cada foto es un testimonio de lo que vivimos.",
+    ],
+   
+    imgs: {
+        "Cada momento contigo es un tesoro inigualable": "./imgs/img1.jpg",
+        "Eres la pieza que altera mi vida.": "./imgs/img6.jpg",
+        "Contigo, cada día es una aventura maravillosa.": "./imgs/img7.jpg",
+        "Tu sonrisa ilumina mi vida.": "./imgs/img3.jpg",
+        "Juntos, creamos recuerdos inolvidables.": "./imgs/img5.jpg",
+        "Eres el caos adorable que amo en mi vida.": "./imgs/img1.jpg",
+        "Hacerte enojar es mi deporte favorito.": "./imgs/img6.jpg",
+        "Prometo seguir queriéndote, incluso cuando te pongas en modo 'drama queen'.": "./imgs/img1.jpg",
+        "No hay nada más hermoso que verte feliz.": "./imgs/img5.jpg",
+        "Te quiero a pesar de tus berrinches.": "./imgs/img2.jpg",
+        "Eres una persona que se gano mi cariño.": "./imgs/img6.jpg",
+        "Espero que te quedes a mi lado por mucho tiempo más. Porfi.": "./imgs/img7.jpg",
+        "Cada foto es un testimonio de lo que vivimos.": "./imgs/img8.jpg",
+    },
+    
+    
+    desc: {
+        turn_on: "click me",
+        play: "como que falta musica",
+        bannar_coming: "hagamos que este mas vivo",
+        balloons_flying: "parece que sigue faltando algo",
+        cake_fadein: "Un pastel？",
+        light_candle: "Me olvide prender la vela :c",
+        wish_message: "Feliz cumpleaños～",
+        story: "espero que te este gustando",
+    },
+
+
+    loveText: 'Te quiero mucho mi niña'
+};
